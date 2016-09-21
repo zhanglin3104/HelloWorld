e2f2f1d5204129f8f6cf5c1ef794a93f03f414af
apple <apple@appledeMacBook-Pro.local>
2016-09-21 18:20:10 +0800

    Finish Homework2

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 
 function print(n:number):void{
 for (var i = 1; i <= num; i++) {
-    var str:string = "";
+    var str = "";
     for (var j = 1; j <= num - i; j++) {
          str = str +" ";
          
     }
     for (var k = 1; k <= 2 * i - 1; k++) {
-         var star :string = "*";
+         var star  = "*";
          str = str + star;
     }
     console.log(str);  
 }
 }
 
- var num:number = process.argv[2]; 
-print(num);
+ var x= process.argv[2]; 
+ var num:number  = parseInt(x);
+ print(num);
 
